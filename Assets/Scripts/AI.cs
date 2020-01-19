9cf5f29de373897882d845c10ee25d17da13afe5
Eric Hu <eric.jie.hu@gmail.com>
2020-01-19 15:02:10 -0800

    Update AI.cs

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -72,7 +72,7 @@ public class AI : MonoBehaviour
         if (crouchTimer > 0)
 		  {
             crouchTimer -= Time.deltaTime;
-        } 
+        }
         if (grabTimer > 0)
 		  {
             grabTimer -= Time.deltaTime;
@@ -125,7 +125,7 @@ public class AI : MonoBehaviour
         //If AI has not been paused
         if (!pauseAI)
 		  {
-            //Down-Left/Right input of of QCF combo
+            //Down-Left/Right input of QCF combo
             if (multiInputTimer <= 0 && finishMove == true)
 				{
                 if (faceLeft == true)
