61514d3b7914f47a3706cd1307dff6e82ed2b9cf
nbaledio <nbaledio@ucsc.edu>
2020-01-30 23:31:23 -0800

    Added highlight effect for characters
    Almost finished. Just need to find a way to deselect a character without bringing up the back menu + pass selected character data to PlayerData.

---
+++ Assets/Scripts/CursorDetection.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorDetection : MonoBehaviour
+{
+    public GameObject[] borders;
+    private bool isOverlap;
+    private string currentChar;
+    private int charNum;
+    public bool P1Selected;
+    public bool P2Selected;
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        currentChar = collider.transform.parent.name;
+        isOverlap = true;
+
+        switch (currentChar)
+        {
+            case "Dhalia":
+                charNum = 0;
+                break;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        borders[charNum].SetActive(false);
+        isOverlap = false;
+    }
+
+    void Update()
+    {
+        if (isOverlap)
+        {
+            borders[charNum].SetActive(true);
+            if (Input.GetButtonDown("Cross_P1") && this.name == "P1Cursor")
+            {
+                P1Selected = true;
+            }
+
+            if (Input.GetButtonDown("Cross_P2") && this.name == "P2Cursor")
+            {
+                P2Selected = true;
+            }
+        }
+
+        //Manage Character Deselection Interactions
+        if (P1Selected && Input.GetButtonDown("Circle_P1"))
+        {
+            P1Selected = false;
+        }
+
+        if (P2Selected && Input.GetButtonDown("Circle_P2"))
+        {
+            P2Selected = false;
+        }
+    }
+}
--- Assets/Scripts/CursorMovement.cs
+++ Assets/Scripts/CursorMovement.cs
@@ -11,39 +11,48 @@ public class CursorMovement : MonoBehaviour {
     private int playerPaused;
     public GameObject P1Cursor;
     public GameObject P2Cursor;
+    public bool P1Selected;
+    public bool P2Selected;
 
     void Update () {
+
         //Set cursor speed to be constant with resolution
         speed = Screen.width/1.5f;
         
+        //Manage Back Menu interations
         if (isPaused == false)
         {
-            //Manage P1Cursor movement
-            float x = Input.GetAxis("Horizontal_P1");
-            float y = Input.GetAxis("Vertical_P1");
+            if (!P1Selected)
+            {
+                //Manage P1Cursor movement
+                float x = Input.GetAxis("Horizontal_P1");
+                float y = Input.GetAxis("Vertical_P1");
 
-            P1Cursor.transform.position += new Vector3(x, y, 0) * Time.deltaTime * speed;
+                P1Cursor.transform.position += new Vector3(x, y, 0) * Time.deltaTime * speed;
 
-            P1Cursor.transform.position = new Vector3(Mathf.Clamp(P1Cursor.transform.position.x, Screen.width/100, Screen.width),
-            Mathf.Clamp(P1Cursor.transform.position.y, Screen.height/20, Screen.height),
-            P1Cursor.transform.position.z);
+                P1Cursor.transform.position = new Vector3(Mathf.Clamp(P1Cursor.transform.position.x, Screen.width / 100, Screen.width),
+                Mathf.Clamp(P1Cursor.transform.position.y, Screen.height / 20, Screen.height),
+                P1Cursor.transform.position.z);
+            }
+            if (!P2Selected)
+            {
+                //Manage P2Cursor movement
+                float x2 = Input.GetAxis("Horizontal_P2");
+                float y2 = Input.GetAxis("Vertical_P2");
 
-           //Manage P2Cursor movement
-            float x2 = Input.GetAxis("Horizontal_P2");
-            float y2 = Input.GetAxis("Vertical_P2");
+                P2Cursor.transform.position += new Vector3(x2, y2, 0) * Time.deltaTime * speed;
 
-            P2Cursor.transform.position += new Vector3(x2, y2, 0) * Time.deltaTime * speed;
-
-            P2Cursor.transform.position = new Vector3(Mathf.Clamp(P2Cursor.transform.position.x, Screen.width / 100, Screen.width),
-            Mathf.Clamp(P2Cursor.transform.position.y, Screen.height / 20, Screen.height),
-            P2Cursor.transform.position.z);
+                P2Cursor.transform.position = new Vector3(Mathf.Clamp(P2Cursor.transform.position.x, Screen.width / 100, Screen.width),
+                Mathf.Clamp(P2Cursor.transform.position.y, Screen.height / 20, Screen.height),
+                P2Cursor.transform.position.z);
+            }
         }
 
         //Manage Back Menu
         if (isPaused)
         {
             ActivateMenu();
-            //Record which player paused
+            //Unpause the game (Only the player that paused can unpause)
             if (Input.GetButtonDown("Circle_P1") && playerPaused == 1)
             {
                 isPaused = !isPaused;
@@ -58,20 +67,18 @@ public class CursorMovement : MonoBehaviour {
         else if (!isPaused)
         {
             DeactivateMenu();
-            //Unpause the game (Only the player that paused can unpause)
-            if (Input.GetButtonDown("Circle_P1") && playerPaused == 0)
+            //Record which player paused
+            if (Input.GetButtonDown("Circle_P1") && playerPaused == 0 && !transform.GetChild(0).GetComponent<CursorDetection>().P1Selected)
             {
                 isPaused = !isPaused;
                 playerPaused = 1;
             }
-            if (Input.GetButtonDown("Circle_P2") && playerPaused == 0)
+            if (Input.GetButtonDown("Circle_P2") && playerPaused == 0 && !P2Selected)
             {
                 isPaused = !isPaused;
                 playerPaused = 2;
             }
         }
-
-
     }
 
     public void ActivateMenu()
