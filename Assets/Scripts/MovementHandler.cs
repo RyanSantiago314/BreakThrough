9dbc366f07e012a1d844c07420d4f343f3add893
jcuneo <jcuneo@ucsc.edu>
2020-02-01 17:23:27 -0800

    fixed input bug

--- Assets/Scripts/MovementHandler.cs
+++ Assets/Scripts/MovementHandler.cs
@@ -78,19 +78,22 @@ public class MovementHandler : MonoBehaviour
         //Original system to use in original Training Stage
         if (SceneManager.GetActiveScene().name == "TrainingStage")
         {
-           Horizontal = "Horizontal_P1";
-            Vertical = "Vertical_P1";
-            L3 = "L3_P1";
-            opponent = GameObject.Find("Player2").transform.GetChild(0).transform;
+            if (transform.parent.name == "Player1")
+            {
+               Horizontal = "Horizontal_P1";
+                Vertical = "Vertical_P1";
+                L3 = "L3_P1";
+                opponent = GameObject.Find("Player2").transform.GetChild(0).transform;
+            }
+            else
+            {
+                Horizontal = "Horizontal_P2";
+                Vertical = "Vertical_P2";
+                L3 = "L3_P2";
+                opponent = GameObject.Find("Player1").transform.GetChild(0).transform;
+            }
+            OpponentProperties = opponent.GetComponent<CharacterProperties>();
         }
-        else
-        {
-            Horizontal = "Horizontal_P2";
-            Vertical = "Vertical_P2";
-            L3 = "L3_P2";
-            opponent = GameObject.Find("Player1").transform.GetChild(0).transform;
-        }
-        OpponentProperties = opponent.GetComponent<CharacterProperties>();
     }
 
     void Start()
