818c14c3192250a79664677a0978b6dd263ef768
goodlygooder <janthomp@ucsc.edu>
2020-02-07 16:16:24 -0800

    Cleaned up controller type detection

--- Assets/Scripts/MenuInputManager.cs
+++ Assets/Scripts/MenuInputManager.cs
@@ -23,281 +23,156 @@ public class MenuInputManager : MonoBehaviour
 	private string state;
 	private bool isXbox;
 
+    private string inputCross = "Cross_P1";
+    private string inputCircle = "Circle_P1";
+    private string inputHorizontal = "Horizontal_P1";
+    private string inputVertical = "Vertical_P1";
+
     // Start is called before the first frame update
     void Start()
     {
-     buttonIndex = 1;
-     InputTimer = 0;
-     xboxInput = "Controller (Xbox One For Windows)";
-     ps4Input = "Wireless Controller";
-     PlayLocalButton.Select();
-     menu = GetComponent<MainMenu>();
-     state = "main";
-     isXbox = false;
-     Time.timeScale = 1;
+        buttonIndex = 1;
+        InputTimer = 0;
+        //xboxInput = "Controller (Xbox One For Windows)";
+        //ps4Input = "Wireless Controller";
+        PlayLocalButton.Select();
+        menu = GetComponent<MainMenu>();
+        state = "main";
+        //isXbox = false;
+        Time.timeScale = 1;
+
+        inputCross += UpdateControls(CheckXbox(0));
+        inputCircle += UpdateControls(CheckXbox(0));
+        inputHorizontal += UpdateControls(CheckXbox(0));
+        inputVertical += UpdateControls(CheckXbox(0));
     }
 
     // Update is called once per frame
     void Update()
     {
-    	horizontal = Input.GetAxis("Horizontal_P1");
-    	vertical = Input.GetAxis("Vertical_P1");
-
+    	horizontal = Input.GetAxis(inputHorizontal);
+    	vertical = Input.GetAxis(inputVertical);
     	if (InputTimer > 0) InputTimer -= Time.deltaTime;
     	else InputTimer = 0;
-
-    	//If an input device is detected then establish what device it is in order to properly decipher inputs
-    	if (Input.GetJoystickNames().Length > 0)
-    	{
-	    	if (Input.GetJoystickNames()[0] == xboxInput && InputTimer == 0)
-	    	{
-	    		if (horizontal < 0)
-	    		{
-	    			buttonIndex += 1;
-	    			InputTimer = 0.1f;
-	    		}
-	    		else if (horizontal > 0)
-	    		{
-	    			buttonIndex -= 1;
-	    			InputTimer = 0.1f;
-	    		}
-	    		isXbox = true;
-	    	}
-	    	else if (Input.GetJoystickNames()[0] == ps4Input && InputTimer == 0)
-	    	{
-	    		if (vertical < 0)
-	    		{
-	    			buttonIndex += 1;
-	    			InputTimer = 0.1f;
-	    		}
-	    		else if (vertical > 0)
-	    		{
-	    			buttonIndex -= 1;
-	    			InputTimer = 0.1f;
-	    		}
-	    		isXbox = false;
-	    	}
-	    	else if (Input.GetJoystickNames()[0] == "" && InputTimer == 0)
-    		{
-    			if (vertical < 0)
-	    		{
-	    			buttonIndex += 1;
-	    			InputTimer = 0.1f;
-	    		}
-	    		else if (vertical > 0)
-	    		{
-	    			buttonIndex -= 1;
-	    			InputTimer = 0.1f;
-	    		}
-	    		isXbox = false;
-    		}
-    	}
-    	else if (Input.GetJoystickNames().Length == 0 && InputTimer == 0)
-    	{
-    		if (vertical < 0)
+        if(InputTimer == 0)
+	    {
+	    	if (vertical < 0)
 	    	{
 	    		buttonIndex += 1;
 	    		InputTimer = 0.1f;
-	    	}
-	    	else if (vertical > 0)
-	    	{
-	    		buttonIndex -= 1;
-	    		InputTimer = 0.1f;
-	    	}
-    	}
-    	
+			}
+			else if (vertical > 0)
+    		{
+    			buttonIndex -= 1;
+    			InputTimer = 0.1f;
+    		}	
+	   	}
     	if (state == "main")
     	{
 	        if (buttonIndex < 1) buttonIndex = 1;
 			else if (buttonIndex > 4) buttonIndex = 4;
-
 			if (buttonIndex == 1)
 			{
 				PlayLocalButton.Select();
-				if (isXbox)
+				if (Input.GetButtonDown(inputCross) || Input.GetButtonDown("Submit"))
 				{
-					if (Input.GetButtonDown("Square_P1") || Input.GetButtonDown("Submit"))
-					{
-						state = "local";
-						buttonIndex = 1;
-						PlayLocalButton.onClick.Invoke();
-					}
-				}
-				else
-				{
-					if (Input.GetButtonDown("Cross_P1") || Input.GetButtonDown("Submit"))
-					{
-						state = "local";
-						buttonIndex = 1;
-						PlayLocalButton.onClick.Invoke();
-					}
+					state = "local";
+					buttonIndex = 1;
+					PlayLocalButton.onClick.Invoke();
 				}
 			}
 			else if (buttonIndex == 2)
 			{
-				PlayOnlineButton.Select();
-				if (isXbox)
-				{
-					if (Input.GetButtonDown("Square_P1") || Input.GetButtonDown("Submit")) PlayOnlineButton.onClick.Invoke();
-				}
-				else
-				{
-					if (Input.GetButtonDown("Cross_P1") || Input.GetButtonDown("Submit")) PlayOnlineButton.onClick.Invoke();
-				}
+				PlayOnlineButton.Select();		
+				if (Input.GetButtonDown(inputCross) || Input.GetButtonDown("Submit")) PlayOnlineButton.onClick.Invoke();
 			}
 			else if (buttonIndex == 3)
 			{
 				OptionsButton.Select();
-				if (isXbox)
+				if (Input.GetButtonDown(inputCross) || Input.GetButtonDown("Submit"))
 				{
-					if (Input.GetButtonDown("Square_P1") || Input.GetButtonDown("Submit"))
-					{
-						state = "options";
-						buttonIndex = 1;
-						OptionsButton.onClick.Invoke();
-					}
-				}
-				else
-				{
-					if (Input.GetButtonDown("Cross_P1") || Input.GetButtonDown("Submit"))
-					{
-						state = "options";
-						buttonIndex = 1;
-						OptionsButton.onClick.Invoke();
-					}
+					state = "options";
+					buttonIndex = 1;
+					OptionsButton.onClick.Invoke();
 				}
 			}
 			else if (buttonIndex == 4)
 			{
 				QuitButton.Select();
-				if (isXbox)
-				{
-					if (Input.GetButtonDown("Square_P1") || Input.GetButtonDown("Submit")) menu.QuitGame();
-				}
-				else
-				{
-					if (Input.GetButtonDown("Cross_P1") || Input.GetButtonDown("Submit")) menu.QuitGame();
-				}
+				if (Input.GetButtonDown(inputCross) || Input.GetButtonDown("Submit")) menu.QuitGame();
 			}
 		}
 		else if (state == "local")
     	{
 	        if (buttonIndex < 1) buttonIndex = 1;
 			else if (buttonIndex > 3) buttonIndex = 3;
-
 			if (buttonIndex == 1)
 			{
 				PlayVsPlayerButton.Select();
-				if (isXbox)
-				{
-					if (Input.GetButtonDown("Square_P1") || Input.GetButtonDown("Submit")) PlayVsPlayerButton.onClick.Invoke();	
-				}
-				else
-				{
-					if (Input.GetButtonDown("Cross_P1") || Input.GetButtonDown("Submit")) PlayVsPlayerButton.onClick.Invoke();
-				}
-				
+				if (Input.GetButtonDown(inputCross) || Input.GetButtonDown("Submit")) PlayVsPlayerButton.onClick.Invoke();	
 			}
 			else if (buttonIndex == 2)
 			{
 				PlayVsAiButton.Select();
-				if (isXbox)
-				{
-					if (Input.GetButtonDown("Square_P1") || Input.GetButtonDown("Submit")) PlayVsAiButton.onClick.Invoke();
-				}
-				else
-				{
-					if (Input.GetButtonDown("Cross_P1") || Input.GetButtonDown("Submit")) PlayVsAiButton.onClick.Invoke();
-				}
+				if (Input.GetButtonDown(inputCross) || Input.GetButtonDown("Submit")) PlayVsAiButton.onClick.Invoke();
 			}
 			else if (buttonIndex == 3)
 			{
 				BackButton.Select();
-				if (isXbox)
-				{
-					if (Input.GetButtonDown("Square_P1") || Input.GetButtonDown("Submit"))
-					{
-						state = "main";
-						buttonIndex = 1;
-						BackButton.onClick.Invoke();
-					}
-				}
-				else
-				{
-					if (Input.GetButtonDown("Cross_P1") || Input.GetButtonDown("Submit"))
-					{
-						state = "main";
-						buttonIndex = 1;
-						BackButton.onClick.Invoke();
-					}
-				}
-			}
-
-			if (isXbox)
-			{
-				if (Input.GetButtonDown("Cross_P1") || Input.GetButtonDown("Cancel"))
+				if (Input.GetButtonDown(inputCross) || Input.GetButtonDown("Submit"))
 				{
 					state = "main";
 					buttonIndex = 1;
 					BackButton.onClick.Invoke();
 				}
 			}
-			else
+			if (Input.GetButtonDown(inputCircle) || Input.GetButtonDown("Cancel"))
 			{
-				if (Input.GetButtonDown("Circle_P1") || Input.GetButtonDown("Cancel"))
-				{
-					state = "main";
-					buttonIndex = 1;
-					BackButton.onClick.Invoke();
-				}
+				state = "main";
+				buttonIndex = 1;
+				BackButton.onClick.Invoke();
 			}
 		}
 		else if (state == "options")
     	{
 	        if (buttonIndex < 1) buttonIndex = 1;
 			else if (buttonIndex > 1) buttonIndex = 1;
-			
 			if (buttonIndex == 1)
 			{
 				OptionsBackButton.Select();
-				if (isXbox)
+				if (Input.GetButtonDown(inputCross) || Input.GetButtonDown("Submit"))
 				{
-					if (Input.GetButtonDown("Square_P1") || Input.GetButtonDown("Submit"))
-					{
-						state = "main";
-						buttonIndex = 3;
-						OptionsBackButton.onClick.Invoke();
-					}
+					state = "main";
+					buttonIndex = 3;
+					OptionsBackButton.onClick.Invoke();
 				}
-				else
-				{
-					if (Input.GetButtonDown("Cross_P1") || Input.GetButtonDown("Submit"))
-					{
-						state = "main";
-						buttonIndex = 3;
-						OptionsBackButton.onClick.Invoke();
-					}
-				}
+			}
+			if (Input.GetButtonDown(inputCircle) || Input.GetButtonDown("Cancel"))
+			{
+				state = "main";
+				buttonIndex = 3;
+				OptionsBackButton.onClick.Invoke();
 			}
 
-			if (isXbox)
-			{
-				if (Input.GetButtonDown("Square_P1") || Input.GetButtonDown("Cancel"))
-				{
-					state = "main";
-					buttonIndex = 3;
-					OptionsBackButton.onClick.Invoke();
-				}
-			}
-			else
-			{
-				if (Input.GetButtonDown("Circle_P1") || Input.GetButtonDown("Cancel"))
-				{
-					state = "main";
-					buttonIndex = 3;
-					OptionsBackButton.onClick.Invoke();
-				}
-			}
 		}
+    }
+
+    private bool CheckXbox(int player)
+    {
+        if (Input.GetJoystickNames().Length > player)
+        {
+            if (Input.GetJoystickNames()[player].Contains("Xbox"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private string UpdateControls(bool xbox)
+    {
+        if (xbox)
+            return "_Xbox";
+        return "";
     }
 }
