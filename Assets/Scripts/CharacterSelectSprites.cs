34d0709ac9b94549d8d11061066941b31dc1cb15
nbaledio <nbaledio@ucsc.edu>
2020-04-10 23:20:43 -0700

    Started adding animated models to character select
    **Was suggested a while back**
    Figured out how to add animated Dhalia + active color swapping to character select. Need to implement logic for player interaction though.

---
+++ Assets/Scripts/CharacterSelectSprites.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectSprites : MonoBehaviour
+{
+    public Animator P1DHAanim;
+    public ColorSwapDHA P1Dhalia;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        P1Dhalia.InitColorSwapTex();
+        P1Dhalia.ApplyPalette();
+    }
+
+    //Blank Functions to prevent errors
+    public void TurnAroundCheck() { }
+    public void EnableAll() { }
+    public void Standing() { }
+    public void ClearHitBox() { }
+}
